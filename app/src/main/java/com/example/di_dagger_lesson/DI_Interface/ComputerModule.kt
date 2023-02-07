a2a206cc4e099148066c137dab334d28407f0f5d
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2023-02-08 00:47:05 +0200

    module added

--- app/src/main/java/com/example/di_dagger_lesson/DI_Interface/ComputerModule.kt
+++ app/src/main/java/com/example/di_dagger_lesson/DI_Interface/ComputerModule.kt
@@ -1,7 +1,14 @@
 package com.example.di_dagger_lesson.DI_Interface
 
+import com.example.dependencyinjectionstart.example1.Monitor
 import dagger.Module
+import dagger.Provides
 
 @Module
 class ComputerModule  {
+
+    @Provides
+    fun provideMonitor():Monitor{
+        return Monitor()
+    }
 }
--- app/src/main/java/com/example/di_dagger_lesson/DI_Interface/NewComponent.kt
+++ app/src/main/java/com/example/di_dagger_lesson/DI_Interface/NewComponent.kt
@@ -3,7 +3,7 @@ package com.example.di_dagger_lesson.DI_Interface
 import com.example.di_dagger_lesson.Activity
 import dagger.Component
 
-@Component
+@Component(modules = [ComputerModule::class])
 interface NewComponent {
 
     fun inject(activity: Activity)
